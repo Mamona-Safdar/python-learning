7b299267de91a2d9a61645c3b82f1f32ca9ada3a
Mamona Safdar <mamonasafdar786@gmail.com>
2026-08-11 10:11:11 +0500

    add car class example(oop)

--- OOP.py
+++ OOP.py
@@ -12,3 +12,21 @@ class Student:
 class CollegeStudent(Student):
           pass
 student1 = CollegeStudent("Memona")
+
+
+
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def drive(self):
+        print("The car is driving")
+
+
+car1 = Car("Toyota", "Red")
+car2 = Car("Honda", "Blue")
+
+print(car1.brand)
+car1.drive()
+
